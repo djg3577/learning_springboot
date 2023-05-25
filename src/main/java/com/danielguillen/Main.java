823756ad2b31156cf51faee0de5a185e0d39e82c
Daniel <Danielguillencoco@yahoo.com>
2023-05-25 08:44:26 -0500

    Auto email dummies generated

--- src/main/java/com/danielguillen/Main.java
+++ src/main/java/com/danielguillen/Main.java
@@ -2,6 +2,8 @@ package com.danielguillen;
 
 import com.danielguillen.customer.Customer;
 import com.danielguillen.customer.CustomerRepository;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
 public class Main {
@@ -22,21 +25,19 @@ public class Main {
         CommandLineRunner runner(CustomerRepository customerRepository){
 
             return args -> {
-                Customer alex = new Customer(
-                        "Alex",
-                        "alex@gmail.com",
-                        21
+                var faker = new Faker();
+                Random random = new Random();
+                Name name = faker.name();
+                String firstName = name.firstName();
+                String lastName = name.lastName();
+                Customer customer = new Customer(
+                        firstName+" "+lastName,
+                        firstName.toLowerCase()+"."+ lastName.toLowerCase()+"@gmail.com",
+                        random.nextInt(16,99)
 
                 );
 
-                Customer Jamila = new Customer(
-                        "Jamila",
-                        "Jamila@gmail.com",
-                        19
-
-                );
-                List<Customer> customers = List.of(alex, Jamila);
-                //customerRepository.saveAll(customers);
+                customerRepository.save(customer);
             };
         }
         @Bean
